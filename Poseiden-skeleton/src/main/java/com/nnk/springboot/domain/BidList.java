cc0c7dcd63b18ab172b3f40ab0a06c13450fdc9f
MarieRodiet <marie.rodiet@gmail.com>
2023-10-13 16:09:11 +0200

    Add fields to entities

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,10 +1,82 @@
 package com.nnk.springboot.domain;
+import lombok.Getter;
+import lombok.Setter;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "bidlist")
+@Getter
+@Setter
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Integer BidListId;
+
+    @Column
+    private String account;
+
+    @Column
+    private String type;
+
+    @Column
+    private Double bidQuantity;
+
+    @Column
+    private Double askQuantity;
+
+    @Column
+    private Double bid;
+
+    @Column
+    private Double ask;
+
+    @Column
+    private String benchmark;
+
+    @Column
+    private Timestamp bidListDate;
+
+    @Column
+    private String commentary;
+
+    @Column
+    private String security;
+
+    @Column
+    private String status;
+
+    @Column
+    private String trader;
+
+    @Column
+    private String book;
+
+    @Column
+    private String creationName;
+
+    @Column
+    private Timestamp creationDate;
+
+    @Column
+    private String revisionName;
+
+    @Column
+    private Timestamp revisionDate;
+
+    @Column
+    private String dealName;
+
+    @Column
+    private String dealType;
+
+    @Column
+    private String sourceListId;
+
+    @Column
+    private String side;
 }
